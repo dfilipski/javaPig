97df3398b8d5942993012a96a7471577bc50c7a7
Dan Filipski <danny.filipski@gmail.com>
2020-12-16 10:28:04 -0500

    Add victory logic

--- src/javaPig/Main.java
+++ src/javaPig/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int turn = 0;
 
         System.out.println("Welcome to pig!");
-        while (true) {
+        while (!isOver(score)) {
             System.out.printf("Player 1 Score: %d\nPlayer 2 Score: %d\n", score[0], score[1]);
             System.out.print("Either [r]oll or [h]old: ");
             char c = sc.nextLine().charAt(0);
@@ -33,7 +33,12 @@ public class Main {
                     System.out.printf("It is now player %d's turn.\n", turn % 2 + 1);
                 }
             }
-
         }
+
+        System.out.printf("The game is over.\nPlayer %d has won.", (turn - 1 ) % 2 + 1);
+    }
+
+    static boolean isOver(int[] score) {
+        return score[0] == 100 || score[1] == 100;
     }
 }
